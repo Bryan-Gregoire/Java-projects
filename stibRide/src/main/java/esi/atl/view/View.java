c454c9017a97e0c76c37753ee0507846e0654b82
Gregoire Bryan <53735@etu.he2b.be>
2021-04-12 00:25:02 +0200

    modify fxml, add DB + config

--- stibRide/src/main/java/esi/atl/view/View.java
+++ stibRide/src/main/java/esi/atl/view/View.java
@@ -2,7 +2,9 @@ package esi.atl.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 /**
  *
@@ -10,9 +12,16 @@ import javafx.fxml.Initializable;
  */
 public class View implements  Initializable   {
 
+    
+    @FXML
+    private Label lblStatus;
+    
+    @FXML
+    private Label lblNbStation;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //TODO
+        
     }
 
 }
